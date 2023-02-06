b55d9fc42442dfc42f7d7a70a1937d02007a2f8f
Hamza Saadi <36114417+sakaspar@users.noreply.github.com>
2023-02-06 13:51:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const fs = require("fs");
+const path = require("path");
 
 const app = express();
 
@@ -7,112 +8,53 @@ const port = 3000;
 
 app.use(express.static("public"));
 
-const filePath = "./videos/SampleVideo_1280x720_1mb.mp4";
-
 app.get("/works-in-chrome", (req, res) => {
-    res.setHeader("content-type", "video/mp4");
-    
-    fs.stat(filePath, (err, stat) => {
+    const videoDir = "./videos";
+
+    fs.readdir(videoDir, (err, files) => {
         if (err) {
-            console.error(`File stat error for ${filePath}.`);
+            console.error(`Error reading video directory ${videoDir}.`);
             console.error(err);
             res.sendStatus(500);
             return;
         }
 
-        res.setHeader("content-length", stat.size);
-
-        const fileStream = fs.createReadStream(filePath);
-        fileStream.on("error", error => {
-            console.log(`Error reading file ${filePath}.`);
-            console.log(error);
-            res.sendStatus(500);
+        const videoFiles = files.filter(file => {
+            return path.extname(file).toLowerCase() === ".mp4";
         });
 
-        fileStream.pipe(res)
-    });
-});
-
-app.get('/works-in-chrome-and-safari', (req, res) => {
-
-    const options = {};
-
-    let start;
-    let end;
-
-    const range = req.headers.range;
-    if (range) {
-        const bytesPrefix = "bytes=";
-        if (range.startsWith(bytesPrefix)) {
-            const bytesRange = range.substring(bytesPrefix.length);
-            const parts = bytesRange.split("-");
-            if (parts.length === 2) {
-                const rangeStart = parts[0] && parts[0].trim();
-                if (rangeStart && rangeStart.length > 0) {
-                    options.start = start = parseInt(rangeStart);
-                }
-                const rangeEnd = parts[1] && parts[1].trim();
-                if (rangeEnd && rangeEnd.length > 0) {
-                    options.end = end = parseInt(rangeEnd);
-                }
-            }
-        }
-    }
-
-    res.setHeader("content-type", "video/mp4");
-
-    fs.stat(filePath, (err, stat) => {
-        if (err) {
-            console.error(`File stat error for ${filePath}.`);
-            console.error(err);
+        if (videoFiles.length === 0) {
+            console.error(`No video files found in ${videoDir}.`);
             res.sendStatus(500);
             return;
         }
 
-        let contentLength = stat.size;
+        const filePath = path.join(videoDir, videoFiles[0]);
 
-        if (req.method === "HEAD") {
-            res.statusCode = 200;
-            res.setHeader("accept-ranges", "bytes");
-            res.setHeader("content-length", contentLength);
-            res.end();
-        }
-        else {        
-            let retrievedLength;
-            if (start !== undefined && end !== undefined) {
-                retrievedLength = (end+1) - start;
-            }
-            else if (start !== undefined) {
-                retrievedLength = contentLength - start;
-            }
-            else if (end !== undefined) {
-                retrievedLength = (end+1);
-            }
-            else {
-                retrievedLength = contentLength;
+        res.setHeader("content-type", "video/mp4");
+        
+        fs.stat(filePath, (err, stat) => {
+            if (err) {
+                console.error(`File stat error for ${filePath}.`);
+                console.error(err);
+                res.sendStatus(500);
+                return;
             }
 
-            res.statusCode = start !== undefined || end !== undefined ? 206 : 200;
+            res.setHeader("content-length", stat.size);
 
-            res.setHeader("content-length", retrievedLength);
-
-            if (range !== undefined) {  
-                res.setHeader("content-range", `bytes ${start || 0}-${end || (contentLength-1)}/${contentLength}`);
-                res.setHeader("accept-ranges", "bytes");
-            }
-
-            const fileStream = fs.createReadStream(filePath, options);
+            const fileStream = fs.createReadStream(filePath);
             fileStream.on("error", error => {
                 console.log(`Error reading file ${filePath}.`);
                 console.log(error);
                 res.sendStatus(500);
             });
-            
+
             fileStream.pipe(res);
-        }
+        });
     });
 });
 
 app.listen(port, () => {
-    console.log(`Open your browser and navigate to http://localhost:${port}`)
+    console.log(`Open your browser and navigate to http://localhost:${port}`);
 });
